01b7f01db1f21b58554949f36c98ba93a8059580
mayureshh27 <143023408+mayureshh27@users.noreply.github.com>
2024-12-31 17:01:02 +0530

    setup done

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,4 +14,17 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
+
+
 export { app }
